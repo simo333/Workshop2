33fbb4a9cecee83de02098022fc16b193fd6280c
Simo <szymon.333@wp.pl>
2022-04-25 18:39:48 +0200

    #Update1
    * Implemented initial version of DBUtil
    * Implemented User entity class
    * Initial version of UserDAO class

---
+++ src/main/java/pl/coderslab/entity/UserDAO.java
@@ -0,0 +1,35 @@
+package pl.coderslab.entity;
+
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.DbUtil;
+
+import java.sql.*;
+
+public class UserDAO {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+}
